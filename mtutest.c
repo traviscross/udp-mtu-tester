3d3d2696c989eec1a4d869daba673ca41e489e6a
Travis Cross <tc@traviscross.com>
2015-09-14 23:23:24 +0000

    Add basic argument validation

--- mtutest.c
+++ mtutest.c
@@ -11,12 +11,18 @@
 #include <netinet/udp.h>
 
 static const int udp_hlen = 28;
+static const int maxbuf = 65535;
+
+int err(char *m) {
+  fprintf(stderr, "error: %s\n", m);
+  return 1;
+}
 
 int main(int argc, char **argv) {
   int udp = socket(AF_INET, SOCK_DGRAM, 0);
   int val = IP_PMTUDISC_DONT;
   struct sockaddr_in addr;
-  char buf[65535];
+  char buf[maxbuf];
   if (argc < 7) {
     fprintf(stderr, "usage: %s ip port startsize maxsize step delay_ms\n", argv[0]);
     return 1;
@@ -27,6 +33,12 @@ int main(int argc, char **argv) {
   int maxsize = atoi(argv[4]);
   int step = atoi(argv[5]);
   int delay_ms = atoi(argv[6]);
+  if (port < 0 || port > 65535) return err("invalid port");
+  if (startsize < udp_hlen) return err("startsize must be at least 28 bytes (UDP header length)");
+  if (maxsize > maxbuf) return err("maxsize must be less than 2^16-1");
+  if (startsize > maxsize) return err("startsize must be less than maxsize");
+  if (step < 0) return err("step must be >=0");
+  if (delay_ms < 0) return err("delay_ms must be >=0");
   memset(&addr, 0, sizeof(addr));
   memset(buf, 0, sizeof(buf));
   addr.sin_family = AF_INET;
